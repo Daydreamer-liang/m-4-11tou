efe705bf106622f20adc3cf5b09382e7b1ac6667
zhuxiangliang <916447016@qq.com>
2021-04-11 07:55:37 +0800

    引入vant 和 rem适配第一段

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 Vue.config.productionTip = false
 
 new Vue({
